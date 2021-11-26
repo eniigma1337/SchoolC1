ad3d0a3749227f0309667d59f23c6017392b70b1
anurag <anuragn.0505@gmail.com>
2021-11-26 18:15:20 -0500

    Add project files.

---
+++ SchoolC1/Controllers/TeacherController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SchoolC1.Models;
+
+namespace SchoolC1.Controllers
+{
+    public class TeacherController : Controller
+    {
+        // GET: Teacher
+        public ActionResult Index()
+        {
+            return View();
+        }
+        //GET : /Teacher/List
+        public ActionResult List(string SearchKey = null)
+        {
+            TeacherDataController controller = new TeacherDataController();
+            IEnumerable<Teacher> Teachers = controller.ListTeachers(SearchKey);
+            return View(Teachers);
+        }
+
+        //GET : /Teacher/SearchResult
+        public ActionResult SearchResult(string SearchKey = null)
+        {
+            TeacherDataController controller = new TeacherDataController();
+            IEnumerable<Teacher> Teachers = controller.ListTeachers(SearchKey);
+            return View(Teachers);
+        }
+
+        //GET : /Teacher/Show/{id}
+        public ActionResult Show(int id)
+        {
+            TeacherDataController controller = new TeacherDataController();
+            IEnumerable<Class> TeacherXClasses = controller.FindTeachersClass(id);
+            Teacher TeacherX = controller.FindTeacher(id);
+            //MAKE A TUPLE TO USE 2 MODELS FOR OUR VIEW
+            return View(Tuple.Create (TeacherX, TeacherXClasses));
+        }
+    }
+}
